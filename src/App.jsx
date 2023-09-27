56ab110b1f1784edce1ee5103c67c4a89be61c6d
Abhishek Santra <abhisheksantra28@gmail.com>
2023-09-27 15:48:28 +0530

    Setup ReduxToolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
-
-import './App.css'
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import "./App.css";
+import authService from "./appwrite/auth";
+import { login, logout } from "./Store/authSlice";
+import { Footer, Header } from "./Components";
+import { Outlet } from "react-router-dom";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
 
-
-
-  return (
-    <>
-      <h1>A blog App with appwrite</h1>
-    </>
-  )
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400 ">
+      <div className="w-full block">
+        <Header />
+        <main>TODO: {/* <Outlet/> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
-export default App
+export default App;
